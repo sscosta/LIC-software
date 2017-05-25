964181d14a47879ce541f441cdfac467676d8488
sscosta <43552@alunos.isel.ipl.pt>
2017-05-25 20:09:20 +0100

    SoundGenerator, Serial Emmitter, LCD, KBD e HAL

--- src/TUI.java
+++ src/TUI.java
@@ -1,8 +1,20 @@
 
 public class TUI {
-
+        char ENTER_KEY = '5';
+        char LEFT_ARROW = '4';
+        char RIGHT_ARROW = '6';
+        char UP_ARROW = '2';
+        char DOWN_ARROW = '0';
+        char DELETE_KEY = '9';
 
     public String getString(int size){
-        
+        int i = size;
+        String str= "";
+        while (KBD.getKey()!=ENTER_KEY||size>0) {
+                char c = 'A';
+                
+            //setas
+            if (KBD.getKey() == '')
+        }
     }
 }
